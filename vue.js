3351472fc2745d019185d549e6ce20be3b5cffca
ibblik <fahm3411@gmail.com>
2020-07-21 13:31:09 +0800

    initialization commit

---
+++ vue.js
@@ -0,0 +1,6 @@
+            var app = new vue ({
+                el: "#app",
+                data : {
+                    "url" : "https://www.fullstackpython.com/img/logos/vuejs-wide.png"
+                }
+            })
